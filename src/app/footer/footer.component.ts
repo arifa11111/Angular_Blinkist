9c1bb739cfaed7b987446c88eb37ab65af295abe
Arifa Shaik <shaik.arifa@zemosolabs.com>
2022-11-17 20:11:45 +0530

    created modal &  modify footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  details = [
+    '../../assets/icons/logo.png',
+    'Big ideas in small packages Start learnign now',
+  ];
+  headings = ['Editorial', 'Useful links', 'Company'];
+  links1 = [
+    'Book lists',
+    'What is Nonfiction?',
+    'What to read next?',
+    'Benefits of reading',
+  ];
+  links2 = [
+    'Pricing',
+    'Blinkist business',
+    'Gift cards',
+    'Blinkist magaine',
+    'Contact & help',
+  ];
+  links3 = ['About', 'Careers', 'partners', 'Code of Conduct'];
+  rights ='© Blinkist 2021 Sitemap   |   Imprint   |   Terms of Service   |   Privacy Policies';
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
