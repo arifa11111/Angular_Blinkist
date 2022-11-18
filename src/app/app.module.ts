84a09a0274a6db4ece5dd6a59f259e77b09da962
ritiC <ritika.chauhan@zemosolabs.com>
2022-11-18 11:17:27 +0530

    header and logo added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
